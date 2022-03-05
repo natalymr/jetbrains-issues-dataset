f5a447cd3eb05b0509ad3b840b616a4b61876849
Natalia Murycheva <natalia.murycheva@jetbrains.com>
2022-03-05 10:24:31 +0400

    partial flat - part 1

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
-from jetbrains_issues_dataset.idea.idea_data_set import idea_2019_03_20_to_idea_2020_03_20
+# from jetbrains_issues_dataset.idea.idea_data_set import idea_2019_03_20_to_idea_2020_03_20
+# from jetbrains_issues_dataset.jetbrains_issues_dataset.idea.idea_data_set import idea_2019_03_20_to_idea_2020_03_20
